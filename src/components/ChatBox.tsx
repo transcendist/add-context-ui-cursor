89d664052264039d92daa90dde0b3b9adb02364d
Dejan Karin <iamtranscendist@gmail.com>
2025-08-27 13:40:31 +0200

    compile error fix

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -16,7 +16,10 @@ interface RenderedMessage {
 }
 
 // Custom hook to detect clicks outside an element
-const useOnClickOutside = (ref: React.RefObject<HTMLElement>, handler: (event: MouseEvent | TouchEvent) => void) => {
+const useOnClickOutside = <T extends HTMLElement>(
+  ref: React.RefObject<T | null>,
+  handler: (event: MouseEvent | TouchEvent) => void
+) => {
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
       if (!ref.current || ref.current.contains(event.target as Node)) {
